d53d3e8152534c0f10ef01ae7727410b17cf1749
Henry Ireland <henryireland95@gmail.com>
2023-09-29 13:30:00 +0100

    finished

--- app.rb
+++ app.rb
@@ -1,1 +1,10 @@
-puts "hello"
+# generate password
+
+a = ("a".."z").to_a
+b = ("A".."Z").to_a
+c = ('0'..'9').to_a
+d = ['£', '@', '*', '&', '$']
+
+password = [a.sample(4)+b.sample(4)+c.sample(2)+d.sample(2)].flatten.shuffle.join("").to_s
+
+print d
